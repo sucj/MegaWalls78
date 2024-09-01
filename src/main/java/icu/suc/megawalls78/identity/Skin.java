0c580495a794db08c3e685a9ea1380cec9eee79e
KKW557 <kevin557557@gmail.com>
2024-09-01 18:35:02 +0800

    1

--- src/main/java/icu/suc/megawalls78/identity/Skin.java
+++ src/main/java/icu/suc/megawalls78/identity/Skin.java
@@ -2,8 +2,9 @@ package icu.suc.megawalls78.identity;
 
 import net.kyori.adventure.text.Component;
 
-public record Skin(String id, String value, String signature) {
-    public Component name() {
-        return Component.translatable("mw78.skin." + id);
+public record Skin(String id, String value, String signature, Component name) {
+
+    public Skin(String id, String value, String signature) {
+        this(id, value, signature, Component.translatable("mw78.skin." + id));
     }
 }
